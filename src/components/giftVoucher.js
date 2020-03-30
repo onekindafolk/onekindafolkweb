c823dbb3bfa2cabb1fa9c6d6e8343e696dd35ef4
Jade Masterson Hally <jademh@gmail.com>
2020-03-30 12:02:03 +0100

    add 30 EUR voucher option

--- src/components/giftVoucher.js
+++ src/components/giftVoucher.js
@@ -74,7 +74,28 @@ export default class GiftVoucher extends Component {
               height="1"
             />
           </Form>
-
+      
+          <Form
+            action="https://www.paypal.com/cgi-bin/webscr"
+            method="post"
+            target="_top"
+          >
+            <input type="hidden" name="cmd" value="_s-xclick" />
+            <input
+              type="hidden"
+              name="hosted_button_id"
+              value="XMYPW6DUQKNAS"
+            />
+            <Button type="submit">€30</Button>
+            <img
+              alt=""
+              border="0"
+              src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif"
+              width="1"
+              height="1"
+            />
+          </Form>
+      
           <Form
             action="https://www.paypal.com/cgi-bin/webscr"
             method="post"
